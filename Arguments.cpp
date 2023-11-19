1b645cd9b8ddfb945fef533829256eff4aeb29b4
Kirill <pinyaev-kirill@ya.ru>
2023-11-19 18:37:58 +0300

    fix error

--- Arguments.cpp
+++ Arguments.cpp
@@ -1,31 +1,27 @@
 #include "Arguments.h"
 
 Arguments::Arguments(int argc, char** argv) {
-  try {
-    for (int i = 1; i < argc; i++) {
-      if (i == 1) {
-        if (std::find(comandlist.begin(), comandlist.end(), argv[i]) !=
-            comandlist.end()) {
-          command = argv[i];
-        } else {
-          vault = argv[i];
-        }
-      } else if (i == 2 && command.empty()) {
-        if (std::find(comandlist.begin(), comandlist.end(), argv[i]) !=
-            comandlist.end()) {
-          command = argv[i];
-        } else {
-          throw std::runtime_error("invalid command: " + std::string(argv[i]));
-        }
+  for (int i = 1; i < argc; i++) {
+    if (i == 1) {
+      if (std::find(comandlist.begin(), comandlist.end(), argv[i]) !=
+          comandlist.end()) {
+        command = argv[i];
       } else {
-        params.push_back(argv[i]);
+        vault = argv[i];
       }
-      data.push_back(argv[i]);
+    } else if (i == 2 && command.empty()) {
+      if (std::find(comandlist.begin(), comandlist.end(), argv[i]) !=
+          comandlist.end()) {
+        command = argv[i];
+      } else {
+        throw std::runtime_error("invalid command: " + std::string(argv[i]));
+      }
+    } else {
+      params.push_back(argv[i]);
     }
-    if (command.empty()) {
-      throw std::runtime_error("No command");
-    }
-  } catch (const std::runtime_error& e) {
-    std::cerr << "Error: " << e.what() << std::endl;
-  };
+    data.push_back(argv[i]);
+  }
+  if (command.empty()) {
+    throw std::runtime_error("No command");
+  }
 };
--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include "Arguments.h"
 
 int main(int argc, char* argv[]) {
-  Arguments args(argc, argv);
-  std::cout << "Vault: " << args.vault << std::endl;
-  std::cout << "Command: " << args.command << std::endl;
-  std::cout << "Args: ";
-  for (auto param : args.params) {
-    std::cout << param << " ";
+  try {
+    Arguments args(argc, argv);
+    std::cout << "Vault: " << args.vault << std::endl;
+    std::cout << "Command: " << args.command << std::endl;
+    std::cout << "Args: ";
+    for (auto param : args.params) {
+      std::cout << param << " ";
+    }
+  } catch (const std::runtime_error& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
   }
   return 0;
 }
